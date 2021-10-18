e233f73d3c8d65a2d687de04c7e97e4ddce40251
yavuzmutlu <yavuzmutlu41@gmail.com>
2021-10-19 00:02:04 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io!")
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+/*euroDun = 11*/
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>" + konutKredileri[index] + "</li>")
+} 
+
+console.log(konutKredileri)
